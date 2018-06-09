c671be12a90a14aefccb1ac0c38f3e9a9ce70757
ZonaClark <zona.clark56@gmail.com>
2018-06-09 14:59:19 -0400

    use middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+
+
+/*
+ *  Use Middleware we can keep track of how the server is working.
+ *  Calling 'next()' will tell express to move on to the next Middleware.
+ */
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log.');
+    }
+  });
+  next();
+});
+
+/*
+ *  Sometimes you don't want to use next() in Middleware.
+ *  When the site is not in maintenance, you can comment it out.
+ *  app.use will be called sequentially, so if you want the public directory to also 
+ *  show maintenance page, move the line 'app.use(express.static(__dirname + '/public'));'
+ *  below it.
+ */
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
